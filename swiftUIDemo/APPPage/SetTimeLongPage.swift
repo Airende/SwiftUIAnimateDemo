dcb9d520b1c0e65618dc3ea280e7e31cfcf81d93
airende <airende@wogoo.onaliyun.com>
2024-08-27 19:30:54 +0800

    111

--- swiftUIDemo/APPPage/SetTimeLongPage.swift
+++ swiftUIDemo/APPPage/SetTimeLongPage.swift
@@ -18,16 +18,17 @@ struct SetTimeLongPage: View {
             Color.clear
                 .ignoresSafeArea()
             AxisSheet(isPresented: $isPresented, constants: constants) {
-                SheetConstants(constants: $constants)
+                SheetTimePointConstants(constants: $constants)
             }
         }
     }
 }
 
 fileprivate
-struct SheetConstants: View {
+struct SheetTimePointConstants: View {
     @Binding var constants: ASConstant
-    
+    @State var timeLong: CGFloat = 0
+    @State var selected: Date = .now
     
     init(constants: Binding<ASConstant>) {
         _constants = constants
@@ -38,7 +39,54 @@ struct SheetConstants: View {
 
     private var formatter: NumberFormatter {
         let f = NumberFormatter()
-        f.numberStyle = .percent
+        f.numberStyle = .decimal
+        f.maximumFractionDigits = 0
+        return f
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(selected.timeIntervalSince1970)")
+                .foregroundColor(Color(hex: "#BFC7D7").opacity(1))
+                .fontWeight(.medium)
+                .font(.system(size: 30))
+                .overlay {
+                    DatePicker("", selection: $selected, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .font(.system(size: 120))
+                        .scaleEffect(5)
+                        .opacity(0.1)
+                        
+                }
+
+            
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color.yellow)
+                .frame(height: 60)
+                .padding(.top, 40)
+                .padding(50)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        
+    }
+}
+
+fileprivate
+struct SheetConstants: View {
+    @Binding var constants: ASConstant
+    @State var timeLong: CGFloat = 0
+    
+    init(constants: Binding<ASConstant>) {
+        _constants = constants
+        UITableView.appearance().backgroundColor = .clear
+    }
+    
+    @State private var value: Double = .zero
+
+    private var formatter: NumberFormatter {
+        let f = NumberFormatter()
+        f.numberStyle = .decimal
         f.maximumFractionDigits = 0
         return f
     }
@@ -47,7 +95,7 @@ struct SheetConstants: View {
         VStack {
             HStack (alignment: .bottom, content: {
                 Group {
-                    Text("13")
+                    Text("\(Int(value))")
                         .fontWeight(.bold)
                         .multilineTextAlignment(.trailing)
                         .font(.system(size: 120))
@@ -58,15 +106,20 @@ struct SheetConstants: View {
                 }
                 .foregroundColor(Color(hex: "#BFC7D7").opacity(1))
             })
+            .padding(.top, 30)
             SlidingRuler(value: $value,
-                         in: 0...1,
-                         step: 0.1,
+                         in: 0...120,
+                         step: 10,
                          snap: .fraction,
                          tick: .fraction,
                          formatter: formatter)
-            Spacer()
+            .padding()
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color.yellow)
+                .frame(height: 60)
+                .padding(50)
         }
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.white)
         
     }
